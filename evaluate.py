9c10d9ef5eab552494f77951ecfb6f91865e9f80
Ali <aliabolghasemi2005@gmail.com>
2026-09-21 14:20:50 +0330

    Add visualization of predictions after model evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,8 @@
 import pathlib
 import keras
+import matplotlib.pyplot as plt
 
-from data import build_dataset
-
+from data import build_dataset, CLASSES
 
 # Path to the trained model
 
@@ -38,3 +38,24 @@ model = keras.models.load_model(
 # Evaluate the model
 
 model.evaluate(test_dataset)
+
+
+# Visualizing the predictions
+
+images, y_true = next(iter(test_dataset))
+
+y_pred = model.predict(
+    images
+)
+
+keras.visualization.plot_bounding_box_gallery(
+    images=images,
+    bounding_box_format="yxyx",
+    y_true=y_true,
+    y_pred=y_pred,
+    rows=2,
+    cols=2,
+    scale=4,
+    class_mapping=CLASSES,
+    legend=True,
+)
